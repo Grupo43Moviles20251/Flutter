97c09bfe7feb2123742fbc8f2e784c17cb8d5d0d
Josue Vega <j.vegav@uniandes.edu.co>
2025-03-07 11:11:28 -0500

    Authentication with firebase sign in completed

---
+++ lib/Pages/signup_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class SignUpPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _SignUpPageState();
+  }
+
+}
+
+class _SignUpPageState extends State<SignUpPage>{
+
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+}
