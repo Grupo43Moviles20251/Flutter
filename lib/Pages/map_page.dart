0923e02df3de3736d650ca2fb4b74e9a8bb7f4a6
Josue Vega <j.vegav@uniandes.edu.co>
2025-03-20 02:03:56 -0300

    Maps page with the map completed

--- lib/Pages/map_page.dart
+++ lib/Pages/map_page.dart
@@ -1,19 +1,32 @@
 import 'package:first_app/Widgets/custom_scaffold.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatelessWidget {
+
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   final int selectedIndex;
   MapPage({this.selectedIndex = 3});
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
       body: Center(
-        child: Text(
-          "Mapa de la zona",  // Aquí puedes agregar tu mapa más tarde
-          style: TextStyle(fontSize: 20),
-        ),
+        child: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+        target: _center,
+        zoom: 11.0,
+    ),
       ),
-      selectedIndex: selectedIndex,
+
+    ), selectedIndex: selectedIndex
     );
   }
 }
